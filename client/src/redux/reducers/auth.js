6e65320d0f6d76f7a0ec26156b0a0fccebe4b682
periakteon <masumgokyuz@gmail.com>
2023-04-02 20:52:07 +0300

    auth reducer added

---
+++ client/src/redux/reducers/auth.js
@@ -0,0 +1,21 @@
+// reducer parametre olarak state ve action alır
+const authReducer = (state = { auth: null }, action) => {
+  switch (action.type) {
+    case 'REGISTER':
+      localStorage.setItem('auth', JSON.stringify(action.payload));
+      return { ...state, auth: action.payload };
+
+      case 'LOGIN':
+      localStorage.setItem('auth', JSON.stringify(action.payload));
+      return { ...state, auth: action.payload };
+
+      case 'LOGOUT':
+      localStorage.clear();
+      return { ...state, auth: null };
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import authReducer from './reducers/auth';
 
 const initialState = {
 
 };
 
 const reducers = combineReducers({
-
+  auth: authReducer
 });
 
 const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(thunk)));
