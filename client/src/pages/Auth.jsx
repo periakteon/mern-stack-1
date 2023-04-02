94e23c06607cfcfbc86d1ccc0be91ee7f39302eb
periakteon <masumgokyuz@gmail.com>
2023-04-02 20:26:07 +0300

    basic typos fixed

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -17,13 +17,13 @@ const Auth = () => {
       <div className='w-1/3 bg-white rounded-md p-3 shadow-md overflow-hidden'>
         <h1 className='text-2xl font-sans font-bold text-indigo-700'>
           {
-            // signUp true dönüyorsa, kayıt ol yazacak, false dönüyorsa giriş yap yazacak.
+            // signUp true dönüyorsa, kayıt ol yazacak; false dönüyorsa giriş yap yazacak.
             signUp ? 'Kayıt Ol' : 'Giriş Yap'
           }
         </h1>
         <div className='flex flex-col space-y-3 my-5 '>
           {
-          // signUp true dönüyorsa, username inputu gösterilecek, false dönüyorsa gösterilmeyecek.
+          // signUp true dönüyorsa, username inputu gösterilecek; false dönüyorsa gösterilmeyecek.
           signUp && <input type='text' value={authData.username} name='username' onChange={onChangeFunction} placeholder='Username' className='input-style block shadow-sm'/>
           }
           <input type='text' value={authData.email} name='email' onChange={onChangeFunction} placeholder='E-mail' className='input-style shadow-sm'/>
@@ -31,14 +31,14 @@ const Auth = () => {
         </div>
         <div className='text-indigo-900 text-xs flex justify-center cursor-pointer mb-4'>
           {
-            // signUp true dönüyorsa, kayıt olmak için tıklayınız yazacak, false dönüyorsa giriş yapmak için tıklayınız yazacak.
+            // signUp true dönüyorsa, kayıt olmak için tıklayınız yazacak; false dönüyorsa giriş yapmak için tıklayınız yazacak.
             signUp ? <span className='transition duration-300 hover:text-blue-700' onClick={() => setSignUp(false)}>Zaten bir hesabınız var mı? Giriş yapmak için tıklayınız.</span> : <span className='transition duration-300 hover:text-blue-700' onClick={() => setSignUp(true)}>Hesabınız yok mu? Kayıt olmak için tıklayınız.</span>
           }
           </div>
         <div>
           <button className='cursor-pointer hover:bg-indigo-800 active:bg-indigo-500 w-full p-2 text-center bg-indigo-600 text-white rounded-md focus:outline-none focus:shadow-outline transition duration-300'>
             {
-            // signUp true dönüyorsa, kayıt ol yazacak, false dönüyorsa giriş yap yazacak.
+            // signUp true dönüyorsa, kayıt ol yazacak; false dönüyorsa giriş yap yazacak.
             signUp ? 'Kayıt Ol' : 'Giriş Yap'
             }
           </button>
