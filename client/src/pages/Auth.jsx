b226458886c6aec0c3fb28f81bc42e9c629e9f90
periakteon <masumgokyuz@gmail.com>
2023-04-02 15:55:43 +0300

    açıklamalar eklendi

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -10,15 +10,26 @@ const Auth = () => {
       <div className='w-1/3 bg-white rounded-md p-3'>
         <h1 className='text-2xl font-sans text-gray-700'>BLOG</h1>
         <div className='flex flex-col space-y-3 my-5 '>
-          {signUp && <input type='text' placeholder='Username' className='input-style'/>}
+          {
+            // signUp true dönüyorsa, username inputu gösterilecek, false dönüyorsa gösterilmeyecek.
+          signUp && <input type='text' placeholder='Username' className='input-style'/>
+          }
           <input type='text' placeholder='E-mail' className='input-style'/>
           <input type='text' placeholder='Password' className='input-style'/>
         </div>
         <div className='text-indigo-900 text-xs flex justify-center cursor-pointer mb-4'>
-          Daha önce giriş yaptınız mı?
+          {
+            // signUp true dönüyorsa, kayıt olmak için tıklayınız yazacak, false dönüyorsa giriş yapmak için tıklayınız yazacak.
+            signUp ? <span>Zaten bir hesabınız var mı? Giriş yapmak için tıklayınız.</span> : <span>Hesabınız yok mu? Kayıt olmak için tıklayınız.</span>
+          }
           </div>
         <div>
-          <button className='cursor-pointer hover:bg-indigo-800 active:bg-indigo-500 w-full p-2 text-center bg-indigo-600 text-white rounded-md'>{signUp ? 'Kayıt Ol' : 'Giriş Yap'}</button>
+          <button className='cursor-pointer hover:bg-indigo-800 active:bg-indigo-500 w-full p-2 text-center bg-indigo-600 text-white rounded-md'>
+            {
+            // signUp true dönüyorsa, kayıt ol yazacak, false dönüyorsa giriş yap yazacak.
+            signUp ? 'Kayıt Ol' : 'Giriş Yap'
+            }
+          </button>
         </div>
       </div>
     </div>
