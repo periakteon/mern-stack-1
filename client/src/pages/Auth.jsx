fabc50ecc5d602fd3a1c2f344bd73033cbfeeb77
periakteon <masumgokyuz@gmail.com>
2023-04-02 15:52:07 +0300

    useState hook'unu kullanarak güzel bir trick: kullanıcı durumuna göre inputları gösteriyoruz

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 
 const Auth = () => {
+
+  const [signUp, setSignUp] = useState(true);
+
+  
   return (
     <div className='w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50'>
       <div className='w-1/3 bg-white rounded-md p-3'>
         <h1 className='text-2xl font-sans text-gray-700'>BLOG</h1>
         <div className='flex flex-col space-y-3 my-5 '>
-          <input type='text' placeholder='Username' className='input-style'/>
+          {signUp && <input type='text' placeholder='Username' className='input-style'/>}
           <input type='text' placeholder='E-mail' className='input-style'/>
           <input type='text' placeholder='Password' className='input-style'/>
         </div>
@@ -14,7 +18,7 @@ const Auth = () => {
           Daha önce giriş yaptınız mı?
           </div>
         <div>
-          <button className='cursor-pointer hover:bg-indigo-800 active:bg-indigo-500 w-full p-2 text-center bg-indigo-600 text-white rounded-md'>Kayıt Ol</button>
+          <button className='cursor-pointer hover:bg-indigo-800 active:bg-indigo-500 w-full p-2 text-center bg-indigo-600 text-white rounded-md'>{signUp ? 'Kayıt Ol' : 'Giriş Yap'}</button>
         </div>
       </div>
     </div>
