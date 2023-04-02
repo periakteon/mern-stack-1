f14adf935dfc14c502001b8ac64e7304b0a81807
periakteon <masumgokyuz@gmail.com>
2023-04-02 16:21:23 +0300

    gradient bg added, shadow added

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -4,31 +4,31 @@ const Auth = () => {
 
   const [signUp, setSignUp] = useState(true);
 
-  
   return (
-    <div className='w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50'>
-      <div className='w-1/3 bg-white rounded-md p-3'>
-        <h1 className='text-2xl font-sans text-gray-700'>
+    <div className='bg-gradient-to-r from-sky-500 to-indigo-500 w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50'>
+      <div className='w-1/3 bg-white rounded-md p-3 shadow-md overflow-hidden'>
+        <h1 className='text-2xl font-sans font-bold text-indigo-700'>
           {
+            // signUp true dönüyorsa, kayıt ol yazacak, false dönüyorsa giriş yap yazacak.
             signUp ? 'Kayıt Ol' : 'Giriş Yap'
           }
         </h1>
         <div className='flex flex-col space-y-3 my-5 '>
           {
           // signUp true dönüyorsa, username inputu gösterilecek, false dönüyorsa gösterilmeyecek.
-          signUp && <input type='text' placeholder='Username' className='input-style'/>
+          signUp && <input type='text' placeholder='Username' className='input-style block shadow-sm'/>
           }
-          <input type='text' placeholder='E-mail' className='input-style'/>
-          <input type='text' placeholder='Password' className='input-style'/>
+          <input type='text' placeholder='E-mail' className='input-style shadow-sm'/>
+          <input type='text' placeholder='Password' className='input-style shadow-sm'/>
         </div>
         <div className='text-indigo-900 text-xs flex justify-center cursor-pointer mb-4'>
           {
             // signUp true dönüyorsa, kayıt olmak için tıklayınız yazacak, false dönüyorsa giriş yapmak için tıklayınız yazacak.
-            signUp ? <span onClick={() => setSignUp(false)}>Zaten bir hesabınız var mı? Giriş yapmak için tıklayınız.</span> : <span onClick={() => setSignUp(true)}>Hesabınız yok mu? Kayıt olmak için tıklayınız.</span>
+            signUp ? <span className='transition duration-300 hover:text-blue-700' onClick={() => setSignUp(false)}>Zaten bir hesabınız var mı? Giriş yapmak için tıklayınız.</span> : <span className='transition duration-300 hover:text-blue-700' onClick={() => setSignUp(true)}>Hesabınız yok mu? Kayıt olmak için tıklayınız.</span>
           }
           </div>
         <div>
-          <button className='cursor-pointer hover:bg-indigo-800 active:bg-indigo-500 w-full p-2 text-center bg-indigo-600 text-white rounded-md'>
+          <button className='cursor-pointer hover:bg-indigo-800 active:bg-indigo-500 w-full p-2 text-center bg-indigo-600 text-white rounded-md focus:outline-none focus:shadow-outline transition duration-300'>
             {
             // signUp true dönüyorsa, kayıt ol yazacak, false dönüyorsa giriş yap yazacak.
             signUp ? 'Kayıt Ol' : 'Giriş Yap'
